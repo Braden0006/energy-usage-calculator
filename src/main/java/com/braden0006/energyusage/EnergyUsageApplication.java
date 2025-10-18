fba3dcda0058e7809a1d7a6268ae22ed60f9b7de
Braden0006 <bradenwhitch@gmail.com>
2025-10-17 21:10:12 -0600

    First commit, set up spring boot project

---
+++ src/main/java/com/braden0006/energyusage/EnergyUsageApplication.java
@@ -0,0 +1,13 @@
+package com.braden0006.energyusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyUsageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyUsageApplication.class, args);
+    }
+
+}
